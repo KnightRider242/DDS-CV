1da5255970dc2b8b7d95a6396a145c4b0be789cc
KnightRider242 <adharshmathew3@gmail.com>
2022-02-10 10:24:46 +0530

    Frame.py comment update

--- Extract_Frames/frame.py
+++ Extract_Frames/frame.py
@@ -5,11 +5,15 @@ path = "..//Data//Video//"+file_name+".avi"
 cap= cv2.VideoCapture(path)
 i=0
 while(cap.isOpened()):
+    # Get the Frame from the video
     ret, frame = cap.read()
     if ret == False:
         break
+    # Write the frame in jpg at the desired folder with iterated frame number
     cv2.imwrite('..//Data//Frames//'+file_name+str(i)+'.jpg',frame)
     i+=1
- 
+ # Close the window / Release webcam
 cap.release() 
+# After we release our webcam, we also release the out-out.release()
+# De-allocate any associated memory usage
 cv2.destroyAllWindows()
